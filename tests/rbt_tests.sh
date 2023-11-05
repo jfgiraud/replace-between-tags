b268ade0f69384c4eed8c29aaea6316ebb5d93eb
jfgiraud <jfgiraud@gmail.com>
2023-11-05 20:06:10 +0100

    Rewrite

--- tests/rbt_tests.sh
+++ tests/rbt_tests.sh
@@ -30,6 +30,57 @@ function assert_exec_equals() {
 
 }
 
+## No tags, no change
+
+cat > /tmp/lorem_ipsum <<'EOF'
+Lorem ipsum dolor sit amet.
+EOF
+
+read -r -d '' EXPECTED <<'EOF'
+Lorem ipsum dolor sit amet.
+EOF
+
+assert_exec_equals \
+    "cd .. ; ./bin/rbt -b '*BEGIN*' -e '*END*' -r 'new text here' < /tmp/lorem_ipsum" \
+    "${EXPECTED}"
+
+rm -f /tmp/lorem_ipsum
+
+## Tags, monoline
+
+cat > /tmp/lorem_ipsum <<'EOF'
+Lorem *BEGIN*ipsum dolor *END*sit amet.
+EOF
+
+read -r -d '' EXPECTED <<'EOF'
+Lorem *BEGIN*new text here*END*sit amet.
+EOF
+
+assert_exec_equals \
+    "cd .. ; ./bin/rbt -b '*BEGIN*' -e '*END*' -r 'new text here' < /tmp/lorem_ipsum" \
+    "${EXPECTED}"
+
+rm -f /tmp/lorem_ipsum
+
+## Tags, monoline
+
+cat > /tmp/lorem_ipsum <<'EOF'
+Lorem *BEGIN*ipsum dolor *END*sit *BEGIN*amet*END*.
+EOF
+
+read -r -d '' EXPECTED <<'EOF'
+Lorem *BEGIN*new text here*END*sit *BEGIN*new text here*END*.
+EOF
+
+assert_exec_equals \
+    "cd .. ; ./bin/rbt -b '*BEGIN*' -e '*END*' -r 'new text here' < /tmp/lorem_ipsum" \
+    "${EXPECTED}"
+
+rm -f /tmp/lorem_ipsum
+
+
+## Tag multiline
+
 cat > /tmp/lorem_ipsum <<'EOF'
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. *BEGIN*Pellentesque maximus faucibus lectus, in ultricies lorem volutpat in.
 Sed rutrum risus et vehicula rhoncus. Nunc sed est et eros mollis vehicula. Pellentesque semper dignissim maximus.
@@ -47,6 +98,30 @@ EOF
 assert_exec_equals \
     "cd .. ; ./bin/rbt -b '*BEGIN*' -e '*END*' -r 'new text here' < /tmp/lorem_ipsum" \
     "${EXPECTED}"
+
+## Tag multiline
+
+cat > /tmp/lorem_ipsum <<'EOF'
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. *BEGIN*
+Pellentesque maximus faucibus lectus, in ultricies lorem volutpat in.
+Sed rutrum risus et vehicula rhoncus. Nunc sed est et eros mollis vehicula. Pellentesque semper dignissim maximus.
+Praesent in justo et ante faucibus eleifend in ac est. Donec orci magna, pellentesque id libero nec, faucibus porta purus.
+Pellentesque luctus sollicitudin tortor sit amet accumsan. Nullam mauris felis, egestas in faucibus in, feugiat vel arcu.
+*END*
+Cras rhoncus aliquam tristique.
+EOF
+
+
+read -r -d '' EXPECTED <<'EOF'
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. *BEGIN*
+new text here*END*
+Cras rhoncus aliquam tristique.
+EOF
+
+assert_exec_equals \
+    "cd .. ; ./bin/rbt -b '*BEGIN*' -e '*END*' -r 'new text here' < /tmp/lorem_ipsum" \
+    "${EXPECTED}"
+
 
 echo "${ok}/${total} (${ko} errors)"
 
